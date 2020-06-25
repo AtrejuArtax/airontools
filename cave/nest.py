f9a04eaf5333be104331a51ceb333ca010bd6525
Atreyu_Artax <claudi_ruiz@hotmail.com>
2020-06-25 21:00:11 +0200

    cleaner code and new methods

--- cave/nest.py
+++ cave/nest.py
@@ -4,6 +4,8 @@ from hyperopt import Trials, STATUS_OK
 import hyperopt
 from tensorflow.keras import callbacks
 from tensorflow.keras import models
+import tensorflow.keras.backend as K
+from tensorflow.keras.optimizers import Adam
 import pickle
 
 
@@ -22,11 +24,12 @@ class DeepNet(object):
             net_name='NN')
 
     def explore(self, x_train, y_train, x_val, y_val, space, model_specs, experiment_specs, path, max_evals,
-                print_model=False, tensor_board=False, metrics=None):
+                print_model=False, tensor_board=False, metrics=None, trials=None):
 
         self.__parallel_models = model_specs['parallel_models']
         self.__device = model_specs['device']
-        trials = Trials()
+        if trials is None:
+            trials = Trials()
 
         def objective(space):
 
@@ -57,6 +60,7 @@ class DeepNet(object):
                 experiment_specs=experiment_specs,
                 mode='exploration',
                 path=path,
+                use_callbacks=True,
                 tensor_board=tensor_board)
 
             # Train and validation losses
@@ -95,8 +99,8 @@ class DeepNet(object):
 
         self.__model = optimize()
 
-    def __train(self, x_train, y_train, x_val, y_val, model, experiment_specs, mode, path, tensor_board=False,
-                use_callbacks=True):
+    def __train(self, x_train, y_train, x_val, y_val, model, experiment_specs, mode, path, use_callbacks,
+                tensor_board=False):
 
         # Callbacks
         callbacks_list = []
@@ -124,16 +128,15 @@ class DeepNet(object):
         # Train model
         class_weight = None if 'class_weight' not in experiment_specs.keys() \
             else {output_name: experiment_specs['class_weight'] for output_name in model.output_names}
-        model.fit(
-            x=x_train,
-            y=y_train,
-            validation_data=(x_val,
-                             y_val),
-            epochs=experiment_specs['epochs'],
-            verbose=0,
-            callbacks=callbacks_list,
-            class_weight=class_weight,
-            shuffle=True)
+        kargs = {'x': x_train,
+                 'y': y_train,
+                 'epochs': experiment_specs['epochs'],
+                 'callbacks': callbacks_list,
+                 'class_weight': class_weight,
+                 'shuffle': True}
+        if not any([val_ is None for val_ in [x_val, y_val]]):
+            kargs.update({'validation_data': (x_val, y_val)})
+        model.fit(**kargs)
 
         # Best model
         if use_callbacks:
@@ -141,7 +144,8 @@ class DeepNet(object):
 
         return model
 
-    def train(self, x_train, y_train, x_val, y_val, experiment_specs, path, tensor_board=False):
+    def train(self, x_train, y_train, experiment_specs, use_callbacks, x_val=None, y_val=None, path=None,
+              tensor_board=False):
 
         # Train model
         self.__model = self.__train(
@@ -153,54 +157,42 @@ class DeepNet(object):
             experiment_specs=experiment_specs,
             mode='training',
             path=path,
-            tensor_board=tensor_board)
+            tensor_board=tensor_board,
+            use_callbacks=use_callbacks)
 
     def inference(self, x_pred):
-
-        # Inference
-        predictions = self.__model.predict(x_pred)
-
-        return predictions
+        return self.__model.predict(x_pred)
 
     def evaluate(self, x, y):
-
-        # Predict
-        eval = self.__model.evaluate(x=x, y=y)
-
-        return eval
+        return self.__model.evaluate(x=x, y=y)
 
     def save_weights(self, filepath):
-
-        # Save weights
         self.__model.save_weights(filepath=filepath)
 
     def load_weights(self, filepath):
-
-        # Load weights
         self.__model.load_weights(filepath=filepath)
 
     def get_weights(self):
-
-        # Get weights
         return self.__model.get_weights()
 
-
     def set_weights(self, weights):
-
-        # Set weights
         self.__model.set_weights(weights=weights)
 
     def save_json(self, filepath):
-
-        # Save json
         model_json = self.__model.to_json()
         with open(filepath, "w") as json_file:
             json_file.write(model_json)
 
     def load_json(self, filepath):
-
-        # Load json
         json_file = open(filepath, 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         self.__model = models.model_from_json(loaded_model_json)
+
+    def clear_session(self):
+        K.clear_session()
+
+    def compile(self, loss, metrics=None, lr=0.001):
+        self.__model.compile(optimizer=Adam(learning_rate=lr),
+                             loss=loss,
+                             metrics=metrics)
