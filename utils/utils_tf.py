7ce6624b00253b62121d48f259691fa6100cdb0e
claudi.ruiz.camps <claudi.ruiz.camps@gmail.com>
2021-05-24 21:38:43 +0200

    utils package created

---
+++ utils/utils_tf.py
@@ -0,0 +1,8 @@
+from tensorflow.keras import models
+import tensorflow.keras.backend as K
+from tensorflow.python.client import device_lib
+
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
