7dfe48e7822862210b09e3d4a440d1d1648cc227
claudi.ruiz.camps <claudi.ruiz.camps@gmail.com>
2021-06-13 13:11:50 +0200

    install dependencies in setup updated

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/AtrejuArtax/airontools',
     packages=setuptools.find_packages(),
+    install_requires=['tensorflow', 'sklearn'],
     classifiers=['Programming Language :: Python :: 3',
                  'License :: BSD 3-Clause',
                  'Operating System :: OS Independent'])
