e477815b2814ba7102c93c35fbae2746cc2671cb
claudi.ruiz.camps <claudi.ruiz.camps@gmail.com>
2021-06-24 16:04:56 +0200

    fixed bugs regarding subpackages

--- airontools/net_constructors/net_constructors_tf.py
+++ airontools/net_constructors/net_constructors_tf.py
@@ -1,5 +1,6 @@
 from tensorflow.keras import regularizers
-from tensorflow.keras.layers import Input, Dense, Concatenate, Reshape, Conv1D, Flatten
+from tensorflow.keras.layers import Concatenate, Reshape, Conv1D, Flatten, LeakyReLU, PReLU, Input, BatchNormalization,\
+    Dense, Dropout, Activation, GRU, Bidirectional, Softmax
 from tensorflow.keras import models
 from tensorflow.python.ops import init_ops
 import tensorflow.keras.backend as K
@@ -7,7 +8,169 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.mixed_precision import experimental as mixed_precision
 from tensorflow.keras.optimizers import Adam
-from airontools.net_constructors.utils_tf import get_layer_units, rm_redundant, custom_block
+from sklearn.metrics import classification_report
+
+
+def custom_block(units, name, specs, input_shape, sequential=False, length=None, bidirectional=False, from_l=1,
+                 activation=None):
+
+    # Hidden layers
+    i_l, o_l = Input(shape=input_shape, name=name + '_input'), None
+    to_l = from_l + len(units)
+    pre_o_dim = None
+    for l, o_dim in zip(range(from_l, to_l), units):
+        if l > from_l:
+            input_shape = (length, pre_o_dim,) if sequential else (pre_o_dim,)
+        else:
+            o_l = i_l
+        o_l = customized_layer(
+            x=o_l,
+            input_shape=input_shape,
+            units=o_dim,
+            activation=specs['hidden_activation'] if activation is None else activation,
+            specs=specs,
+            name=name,
+            l=l,
+            sequential=sequential,
+            return_sequences=True if l < to_l - 1 and sequential else False,
+            bidirectional=bidirectional)
+        pre_o_dim = o_dim
+
+    # Model
+    model = models.Model(inputs=i_l, outputs=o_l)
+
+    return model
+
+
+def customized_layer(x, input_shape, units, activation, specs, name, l, dropout=True, return_sequences=False,
+                     sequential=False, bidirectional=False):
+
+    # Dropout
+    if dropout:
+        x = Dropout(
+            name=name + '_dropout_' + str(l),
+            rate=specs['dropout_rate'],
+            input_shape=input_shape)(x)
+
+    # Recurrent
+    if sequential:
+        if bidirectional:
+            x = Bidirectional(GRU(
+                name=name + '_gru_' + str(l),
+                input_shape=input_shape,
+                units=units,
+                use_bias=True,
+                kernel_initializer=init_ops.random_normal_initializer(),
+                bias_initializer=init_ops.zeros_initializer(),
+                kernel_regularizer=regularizers.l1_l2(
+                    l1=specs['kernel_regularizer_l1'],
+                    l2=specs['kernel_regularizer_l2']),
+                bias_regularizer=regularizers.l1_l2(
+                    l1=specs['bias_regularizer_l1'],
+                    l2=specs['bias_regularizer_l2']),
+                return_sequences=return_sequences,
+                activation='linear'),
+            input_shape=input_shape)(x)
+        else:
+            x = GRU(
+                name=name + '_gru_' + str(l),
+                input_shape=input_shape,
+                units=units,
+                use_bias=True,
+                kernel_initializer=init_ops.random_normal_initializer(),
+                bias_initializer=init_ops.zeros_initializer(),
+                kernel_regularizer=regularizers.l1_l2(
+                    l1=specs['kernel_regularizer_l1'],
+                    l2=specs['kernel_regularizer_l2']),
+                bias_regularizer=regularizers.l1_l2(
+                    l1=specs['bias_regularizer_l1'],
+                    l2=specs['bias_regularizer_l2']),
+                return_sequences=return_sequences,
+                activation='linear')(x)
+
+    # Dense
+    else:
+        x = Dense(
+            name=name + '_dense_' + str(l),
+            input_shape=input_shape,
+            units=units,
+            use_bias=True,
+            kernel_initializer=init_ops.random_normal_initializer(),
+            bias_initializer=init_ops.zeros_initializer(),
+            kernel_regularizer=regularizers.l1_l2(
+                l1=specs['kernel_regularizer_l1'],
+                l2=specs['kernel_regularizer_l2']),
+            bias_regularizer=regularizers.l1_l2(
+                l1=specs['bias_regularizer_l1'],
+                l2=specs['bias_regularizer_l2']))(x)
+
+    # Batch Normalization
+    if specs['bn']:
+        x = BatchNormalization(
+            name=name + '_batch_normalization_' + str(l),
+            input_shape=input_shape)(x)
+
+    # Activation
+    if activation == 'leakyrelu':
+        x = LeakyReLU(
+            name = name + '_' + activation + '_' + str(l),
+            input_shape=input_shape,
+            alpha=specs['alpha'])(x)
+    elif activation == 'prelu':
+        x = PReLU(
+            name=name + '_' + activation + '_' + str(l),
+            input_shape=input_shape,
+            alpha_regularizer=regularizers.l1_l2(
+                l1=specs['bias_regularizer_l1'],
+                l2=specs['bias_regularizer_l2']))(x)
+    elif activation == 'softmax':
+        x = Softmax(
+            name=name + '_' + activation + '_' + str(l),
+            input_shape=input_shape,
+            dtype='float32')(x)
+    else:
+        x = Activation(
+            name=name + '_' + activation + '_' + str(l),
+            input_shape=input_shape,
+            activation=activation)(x)
+
+    return x
+
+
+def to_time_series(tensor):
+    return K.expand_dims(tensor, axis=2)
+
+
+def evaluate_clf(cat_encoder, model, x, y):
+    pred = inference(cat_encoder, model, x)
+    print("\nReport:")
+    print(classification_report(y, pred, digits=4))
+
+
+def inference(cat_encoder, model, x):
+    inf = model.predict(x)
+    if isinstance(inf, list):
+        inf = [sub_inf.reshape(sub_inf.shape + tuple([1])) for sub_inf in inf]
+        inf = np.concatenate(tuple(inf), axis=-1)
+        inf = np.mean(inf, axis=-1)
+    return cat_encoder.inverse_transform(inf)
+
+
+def get_layer_units(input_dim, output_dim, n_layers):
+    n_units = input_dim if input_dim >= output_dim else output_dim
+    return [int(units) for units in np.linspace(n_units, output_dim, n_layers + 1)]
+
+
+def rm_redundant(values, value):
+    taken = False
+    values_ = []
+    for n in values:
+        if n != value:
+            values_ += [n]
+        elif not taken:
+            values_ += [n]
+            taken = True
+    return values_
 
 
 def net_constructor(input_specs, output_specs, devices, net_name='', compile_model=True, metrics=None, lr=0.001, **specs):
--- airontools/preprocessing.py
+++ airontools/preprocessing.py
@@ -2,6 +2,75 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
+from sklearn.model_selection import KFold
+
+
+def sub_sample(data, n):
+    data_ = data.copy()
+    data_.index = np.arange(data_.shape[0])
+    return data.loc[:n-1, data_.columns]
+
+
+def array_to_list(input_data, output_data, n_parallel_models, data_specs, do_kfolds=False, val_ratio=0.2,
+                      shuffle=True, seed_val=0):
+    """From array to list of numpy.
+
+        Parameters:
+            input_data (np.ndarray): Input data.
+            output_data (np.ndarray): Output data.
+            n_parallel_models (int): Number of parallel models.
+            data_specs (dict): Dataset specifications.
+            do_kfolds (bool): Whether to do kfolds for cross-validation or not.
+            val_ratio (float): Ratio for validation.
+            shuffle (bool): Whether to shuffle or not.
+            seed_val (int): Seed value.
+
+        Returns:
+            4 lists.
+    """
+    x_train, x_val, y_train, y_val = [], [], [], []
+    if do_kfolds and n_parallel_models > 1:
+        kf = KFold(n_splits=n_parallel_models, shuffle=True, random_state=seed_val)
+        n_train = min([data[0].shape[0] for data in kf.split(range(input_data.shape[0]))])
+        n_val = min([data[1].shape[0] for data in kf.split(range(input_data.shape[0]))])
+        train_val_inds = [[train_inds[:n_train, ...], val_inds[:n_val, ...]]
+                          for train_inds, val_inds in kf.split(range(input_data.shape[0]))]
+    else:
+        inds = np.arange(0, input_data.shape[0])
+        if shuffle:
+            import random
+            from random import seed
+            random.shuffle(inds, random=seed(seed_val))
+        line = int(len(inds) * (1 - val_ratio))
+        train_val_inds = [[inds[0:line], inds[line:]] for _ in np.arange(0, n_parallel_models)]
+    for train_inds, val_inds in train_val_inds:
+        x_train += [input_data[train_inds, ...]]
+        if val_ratio > 0:
+            x_val += [input_data[val_inds, ...]]
+        y_train += [output_data[train_inds, ...]]
+        if val_ratio > 0:
+            y_val += [output_data[val_inds, ...]]
+
+    return x_train, x_val, y_train, y_val, train_val_inds
+
+
+def update_specs(data_specs, input_data, output_data, cat_dictionary):
+    """Update specs given data specs and input and output data.
+
+        Parameters:
+            data_specs (dict): Data specifications.
+            input_data (pd.DataFrame): Input data.
+            output_data (pd.DataFrame): Output data.
+            cat_dictionary (pd.DataFrame): Categorical dictionary.
+    """
+    for specs_name, prep_data in zip(data_specs.keys(), [input_data, output_data]):
+        specs = data_specs[specs_name]
+        for feature_name, feature_specs in specs.items():
+            dim = prep_data[feature_name][0].shape[-1] if not feature_specs['type'] == 'cat' \
+                else len(cat_dictionary[feature_name + '_dictionary'][0])
+            dim = 1 if dim == 2 and feature_specs['type'] == 'cat' and specs_name != 'output_specs' else dim
+            feature_specs.update({'dim': dim})
+
 
 
 class PreProcessing(object):
