0bf4d901736ed1d17d3b65472f0acd162ccaef29
claudi.ruiz.camps <claudi.ruiz.camps@gmail.com>
2021-10-12 12:05:54 +0200

    removed empty lines

--- airontools/visualization/tensorboard_tf.py
+++ airontools/visualization/tensorboard_tf.py
@@ -5,5 +5,3 @@ from airontools.net_constructors.utils_tf import get_latent_model
 def get_insights(x, model, hidden_layer_names=None, **kwargs):
     model_ = model if not hidden_layer_names else get_latent_model(model, hidden_layer_names)
     save_insights(embeddings=model_.predict(x), **kwargs)
-
-
