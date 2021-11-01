97db29c31e5d54341ad4516d2ffb0292f872f04e
claudi.ruiz.camps <claudi.ruiz.camps@gmail.com>
2021-11-01 16:46:27 +0100

    changed order in __init__

--- airontools/model_constructors/__init__.py
+++ airontools/model_constructors/__init__.py
@@ -1,8 +1,8 @@
 from airontools.backend import get_backend
 BACKEND = get_backend()
-if BACKEND == 'tensorflow':
-    from airontools.model_constructors.constructors_tf import *
-    from airontools.model_constructors.utils_tf import *
-else:
+if BACKEND == 'pytorch':
     from airontools.model_constructors.constructors_torch import *
     from airontools.model_constructors.utils_torch import *
+else:
+    from airontools.model_constructors.constructors_tf import *
+    from airontools.model_constructors.utils_tf import *
--- airontools/model_interactors/__init__.py
+++ airontools/model_interactors/__init__.py
@@ -1,6 +1,6 @@
 from airontools.backend import get_backend
 BACKEND = get_backend()
-if BACKEND == 'tensorflow':
-    from airontools.model_interactors.model_interactors_tf import *
-else:
+if BACKEND == 'pytorch':
     from airontools.model_interactors.model_interactors_torch import *
+else:
+    from airontools.model_interactors.model_interactors_tf import *
