f1ad80fa82122b8459deb8a7a02a27f545bb06a1
claudi <claudi.ruiz.camps@gmail.com>
2021-11-15 14:47:50 +0100

    renamed devices packages

--- airontools/devices/__init__.py
+++ airontools/devices/__init__.py
@@ -1,6 +1,6 @@
 from airontools.backend import get_backend
 BACKEND = get_backend()
 if BACKEND == 'tensorflow':
-    from airontools.devices.utils_tf import *
+    from airontools.devices.devices_tf import *
 else:
-    from airontools.devices.utils_torch import *
+    from airontools.devices.devices_torch import *
