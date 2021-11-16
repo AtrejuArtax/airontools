9d489d9afa10a7cea0b990ab6a7f8b112b7e2461
claudi <claudi.ruiz.camps@gmail.com>
2021-11-16 17:51:12 +0100

    fixed import issue

--- airontools/constructors/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from airontools.backend import get_backend
-BACKEND = get_backend()
-from airontools.constructors.models.general import *
-from airontools.constructors.models.supervised import *
-from airontools.constructors.models.unsupervised import *
--- airontools/constructors/models/supervised/__init__.py
+++
@@ -1,3 +0,0 @@
-from airontools.backend import get_backend
-BACKEND = get_backend()
-from airontools.constructors.models.supervised.classification import *
