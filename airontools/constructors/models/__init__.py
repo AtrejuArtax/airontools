cac8c439fee423f9bd50541801d168559bbf12ae
claudi <claudi.ruiz.camps@gmail.com>
2021-11-16 16:34:23 +0100

    included sub classes for different type of models

--- airontools/constructors/models/__init__.py
+++ airontools/constructors/models/__init__.py
@@ -1,6 +1,5 @@
 from airontools.backend import get_backend
 BACKEND = get_backend()
-if BACKEND == 'pytorch':
-    from airontools.constructors.models.models_torch import *
-else:
-    from airontools.constructors.models.models_tf import *
+from airontools.constructors.models.general import *
+from airontools.constructors.models.supervised import *
+from airontools.constructors.models.unsupervised import *
---
+++ airontools/constructors/models/supervised/__init__.py
@@ -0,0 +1,3 @@
+from airontools.backend import get_backend
+BACKEND = get_backend()
+from airontools.constructors.models.supervised.classification import *
