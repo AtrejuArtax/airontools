090e6eeed3ffec673d3da97699e25d8e92ebc205
claudi <claudi.ruiz.camps@gmail.com>
2021-12-12 13:47:43 +0100

    extended input cases for the train_val_split function

--- airontools/preprocessing.py
+++ airontools/preprocessing.py
@@ -16,33 +16,34 @@ def sub_sample(data, n):
 
 
 def train_val_split(input_data, output_data=None, meta_data=None, n_parallel_models=1, do_kfolds=False, val_ratio=0.2,
-                    shuffle=True, seed_val=None, to_tfrecord=False, tfrecord_name=None):
+                    shuffle=True, seed_val=None, return_tfrecord=False, tfrecord_name=None, batch_size=None):
     """ Train validation split.
 
         Parameters:
-            input_data (np.ndarray): Input data.
-            output_data (np.ndarray): Output data.
-            meta_data (np.ndarray): Meta data.
+            input_data (list[array], array): Input data.
+            output_data (list[array], array): Output data.
+            meta_data (list[array], array): Meta data.
             n_parallel_models (int): Number of parallel models.
             do_kfolds (bool): Whether to do kfolds for cross-validation or not.
             val_ratio (float): Ratio for validation.
             shuffle (bool): Whether to shuffle or not.
             seed_val (int): Seed value.
-            to_tfrecord (bool): Whether to convert the outputs to tfrecord or not.
+            return_tfrecord (bool): Whether to return tfrecord or not.
             tfrecord_path (str): Name of the tfrecord.
+            batch_size (int): Batch size used for the tfrecord dataset.
 
         Returns:
             4 list[np.ndarray] or list[tfrecord].
     """
     n_samples = input_data.shape[0]
     distributions = ['train', 'val']
-    data = dict(x=input_data)
+    data = dict(x=input_data if isinstance(input_data, list) else [input_data])
     split_data = dict(x={distribution: [] for distribution in distributions})
     if output_data is not None:
-        data.update(y=output_data)
+        data.update(y=output_data if isinstance(output_data, list) else [output_data])
         split_data.update(y={distribution: [] for distribution in distributions})
     if meta_data is not None:
-        data.update(meta=meta_data)
+        data.update(meta=meta_data if isinstance(meta_data, list) else [meta_data])
         split_data.update(meta={distribution: [] for distribution in distributions})
     if do_kfolds and n_parallel_models > 1:
         kf = KFold(
@@ -62,23 +63,33 @@ def train_val_split(input_data, output_data=None, meta_data=None, n_parallel_mod
         inds = [dict(train=inds[:line], val=inds[line:]) for _ in np.arange(0, n_parallel_models)]
     for inds_ in inds:
         for distribution in distributions:
-            split_data['x'][distribution] += [data['x'][inds_[distribution], ...]]
+            x_ = []
+            for sub_x in data['x']:
+                x_ += [sub_x[inds_[distribution], ...]]
+            split_data['x'][distribution] += [x_]
             if output_data is not None:
-                split_data['y'][distribution] += [data['y'][inds_[distribution], ...]]
+                y_ = []
+                for sub_y in data['y']:
+                    y_ += [sub_y[inds_[distribution], ...]]
+                split_data['y'][distribution] += [y_]
             if meta_data is not None:
-                split_data['meta'][distribution] += [data['meta'][inds_[distribution], ...]]
-    if to_tfrecord:
+                meta_ = []
+                for sub_meta in data['meta']:
+                    meta_ += [sub_meta[inds_[distribution], ...]]
+                split_data['meta'][distribution] += [meta_]
+    if return_tfrecord:
         if tfrecord_name is None:
             tfrecord_name = os.path.join(tempfile.gettempdir(), 'tfrecord')
         for split_data_name in split_data.keys():
             for distribution in distributions:
                 for i in range(n_parallel_models):
-                    tfrecord_name_ = '_'.join([tfrecord_name, split_data_name, distribution, str(i)]) + '.tfrecords'
-                    write_tfrecord(
-                        data=split_data[split_data_name][distribution][i],
-                        name=tfrecord_name_
-                    )
-                    split_data[split_data_name][distribution][i] = [read_tfrecord(tfrecord_name_)]
+                    for j in range(split_data[split_data_name][distribution][i]):
+                        tfrecord_name_ = '_'.join([tfrecord_name, split_data_name, distribution, str(i)]) + '.tfrecords'
+                        write_tfrecord(
+                            data=split_data[split_data_name][distribution][i][j],
+                            name=tfrecord_name_
+                        )
+                        split_data[split_data_name][distribution][i] = [read_tfrecord(tfrecord_name_)]
     returns = []
     for split_data_name in split_data.keys():
         for distribution in distributions:
@@ -86,6 +97,7 @@ def train_val_split(input_data, output_data=None, meta_data=None, n_parallel_mod
             if n_parallel_models == 1:
                 return_ = return_[0]
             returns += [return_]
+    returns += [inds]
     return returns
 
 
@@ -96,18 +108,24 @@ def write_tfrecord(data, name):
             writer.write(tf_example.SerializeToString())
 
 
-def read_tfrecord(name):
+def read_tfrecord(name, batch_size=None):
     dataset = tf.data.TFRecordDataset(name)
-    return dataset.map(parse_function)
+    if batch_size is not None:
+        dataset = dataset.batch(batch_size)
+    dataset = dataset.map(parse_function)
+    return dataset
 
 
 def parse_function(example_proto):
-    feature_description = {'dataset': tf.io.FixedLenFeature([], tf.string)}
-    return tf.io.parse_single_example(example_proto, feature_description)
+    feature_description = {'data': tf.io.FixedLenFeature([], tf.string)}
+    example = tf.io.parse_single_example(example_proto, feature_description)
+    data = example['data']
+    tf.image.decode_jpeg()
+    return
 
 
 def example(data):
-    feature = {'dataset': tf.train.Feature(bytes_list=tf.train.BytesList(value=[data.tobytes()]))}
+    feature = {'data': tf.train.Feature(bytes_list=tf.train.BytesList(value=[data.tobytes()]))}
     return tf.train.Example(features=tf.train.Features(feature=feature))
 
 
