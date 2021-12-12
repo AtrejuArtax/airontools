b99b17a92192a89b22793d9f6bb22141bdf8dfab
claudi <claudi.ruiz.camps@gmail.com>
2021-12-12 12:25:10 +0100

    simplified code regarding train_val_split, and it is compatible with tfrecords

--- airontools/preprocessing.py
+++ airontools/preprocessing.py
@@ -1,8 +1,11 @@
+import os
 import random
+import tempfile
 from random import seed
 
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from sklearn.model_selection import KFold
 
 
@@ -13,7 +16,7 @@ def sub_sample(data, n):
 
 
 def train_val_split(input_data, output_data=None, meta_data=None, n_parallel_models=1, do_kfolds=False, val_ratio=0.2,
-                    shuffle=True, seed_val=None):
+                    shuffle=True, seed_val=None, to_tfrecord=False, tfrecord_name=None):
     """ Train validation split.
 
         Parameters:
@@ -25,77 +28,78 @@ def train_val_split(input_data, output_data=None, meta_data=None, n_parallel_mod
             val_ratio (float): Ratio for validation.
             shuffle (bool): Whether to shuffle or not.
             seed_val (int): Seed value.
+            to_tfrecord (bool): Whether to convert the outputs to tfrecord or not.
+            tfrecord_path (str): Name of the tfrecord.
 
         Returns:
-            4 lists.
+            4 list[np.ndarray] or list[tfrecord].
     """
     # ToDo: Simplify code
-    x_train, x_val, y_train, y_val, meta_train, meta_val = [], [], [], [], [], []
+    # x_train, x_val, y_train, y_val, meta_train, meta_val = [], [], [], [], [], []
+    n_samples = input_data.shape[0]
+    distributions = ['train', 'val']
+    data = dict(x=input_data)
+    split_data = dict(x={distribution: [] for distribution in distributions})
+    if output_data is not None:
+        data.update(y=output_data)
+        split_data.update(y={distribution: [] for distribution in distributions})
+    if meta_data is not None:
+        data.update(meta=meta_data)
+        split_data.update(meta={distribution: [] for distribution in distributions})
     if do_kfolds and n_parallel_models > 1:
-        kf = KFold(n_splits=n_parallel_models, shuffle=True, random_state=seed_val)
-        n_train = min([data[0].shape[0] for data in kf.split(range(input_data.shape[0]))])
-        n_val = min([data[1].shape[0] for data in kf.split(range(input_data.shape[0]))])
-        train_val_inds = [[train_inds[:n_train, ...], val_inds[:n_val, ...]]
-                          for train_inds, val_inds in kf.split(range(input_data.shape[0]))]
+        kf = KFold(
+            n_splits=n_parallel_models,
+            shuffle=True,
+            random_state=seed_val
+        )
+        n_train = min([data_[0].shape[0] for data_ in kf.split(range(n_samples))])
+        n_val = min([data_[1].shape[0] for data_ in kf.split(range(n_samples))])
+        inds = [dict(train=train_inds[:n_train, ...], val=val_inds[:n_val, ...])
+                for train_inds, val_inds in kf.split(range(n_samples))]
     else:
-        inds = np.arange(0, input_data.shape[0])
+        inds = list(np.arange(n_samples))
         if shuffle:
             random.shuffle(inds, random=seed(seed_val))
         line = int(len(inds) * (1 - val_ratio))
-        train_val_inds = [[inds[0:line], inds[line:]] for _ in np.arange(0, n_parallel_models)]
-    for train_inds, val_inds in train_val_inds:
-        x_train += [input_data[train_inds, ...]]
-        if val_ratio > 0:
-            x_val += [input_data[val_inds, ...]]
-        if output_data is not None:
-            y_train += [output_data[train_inds, ...]]
-        if meta_data is not None:
-            meta_train += [meta_data[train_inds, ...]]
-        if val_ratio > 0:
+        inds = [dict(train=inds[:line], val=inds[line:]) for _ in np.arange(0, n_parallel_models)]
+    for inds_ in inds:
+        for distribution in distributions:
+            split_data['x'][distribution] += [data['x'][inds_[distribution], ...]]
             if output_data is not None:
-                y_val += [output_data[val_inds, ...]]
+                split_data['y'][distribution] += [data['y'][inds_[distribution], ...]]
             if meta_data is not None:
-                meta_val += [meta_data[val_inds, ...]]
-    if n_parallel_models == 1:
-        if len(x_train) > 0:
-            x_train = x_train[0]
-        if len(x_val) > 0:
-            x_val = x_val[0]
-    returns = [x_train, x_val]
-    if output_data is not None:
-        if n_parallel_models == 1:
-            if len(y_train) > 0:
-                y_train = y_train[0]
-            if len(y_val) > 0:
-                y_val = y_val[0]
-        returns += [y_train, y_val]
-    if meta_data is not None:
-        if n_parallel_models == 1:
-            if len(meta_train) > 0:
-                meta_train = meta_train[0]
-            if len(meta_val) > 0:
-                meta_val = meta_val[0]
-        returns += [meta_train, meta_val]
-    returns += [train_val_inds]
+                split_data['meta'][distribution] += [data['meta'][inds_[distribution], ...]]
+    if to_tfrecord:
+        if tfrecord_name is None:
+            tfrecord_name = os.path.join(tempfile.gettempdir(), 'tfrecord')
+        for split_data_name in split_data.keys():
+            for distribution in distributions:
+                for i in range(n_parallel_models):
+                    write_tfrecord(
+                        data=split_data[split_data_name][distribution][i],
+                        name='_'.join([tfrecord_name, split_data_name, distribution, str(i)])
+                    )
+                    split_data[split_data_name][distribution][i] = []
+    returns = []
+    for split_data_name in split_data.keys():
+        for distribution in distributions:
+            return_ = split_data[split_data_name][distribution]
+            if n_parallel_models == 1:
+                return_ = return_[0]
+            returns += [return_]
     return returns
 
 
-def update_specs(data_specs, input_data, output_data, cat_dictionary):
-    """ Update specs given data specs and input and output data.
+def write_tfrecord(data, name):
+    with tf.io.TFRecordWriter(name + '.tfrecords') as writer:
+        for i in range(len(data)):
+            tf_example = __example(data[i])
+            writer.write(tf_example.SerializeToString())
 
-        Parameters:
-            data_specs (dict): Data specifications.
-            input_data (pd.DataFrame): Input data.
-            output_data (pd.DataFrame): Output data.
-            cat_dictionary (pd.DataFrame): Categorical dictionary.
-    """
-    for specs_name, prep_data in zip(data_specs.keys(), [input_data, output_data]):
-        specs = data_specs[specs_name]
-        for feature_name, feature_specs in specs.items():
-            dim = prep_data[feature_name][0].shape[-1] if not feature_specs['type'] == 'cat' \
-                else len(cat_dictionary[feature_name + '_dictionary'][0])
-            dim = 1 if dim == 2 and feature_specs['type'] == 'cat' and specs_name != 'output_specs' else dim
-            feature_specs.update({'dim': dim})
+
+def __example(data):
+    feature = {'dataset': tf.train.Feature(bytes_list=tf.train.BytesList(value=[data.tobytes()]))}
+    return tf.train.Example(features=tf.train.Features(feature=feature))
 
 
 def to_time_series(dataset, targets, look_back=1):
