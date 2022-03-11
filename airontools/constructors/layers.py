3ee2f2b2e975670024cfb218f94d2f70cb55ef73
chirag32527 <chiragworks@yahoo.in>
2022-03-11 20:35:00 +0530

    pr changes - changing custom dense layer to custom layer
    Co-authored-by: AtrejuArtax <claudi_ruiz@hotmail.com>

--- airontools/constructors/layers.py
+++ airontools/constructors/layers.py
@@ -69,7 +69,6 @@ def layer_constructor(x,
             images and 4 for 3D images).
             advanced_reg (bool): Whether to automatically set advanced regularization. Useful to quickly make use of all
             the regularization properties.
-            is_activated_custom (bool): Whether to use a custom Dense layer with keras Functional API or not
             custom_layer (Layer): Custom Layer class constructed with keras Functional API
             if is_activated_custom=True
             dropout_rate (float): Probability of each intput being disconnected.
