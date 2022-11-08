f42ffc1555b96cc721f2b9065e0c7bd1cb172c4b
claudi <claudi_ruiz@hotmail.com>
2022-11-08 18:53:23 +0100

    unit test for layer constructor

---
+++ tests/constructors/models/test_layerss.py
@@ -0,0 +1,11 @@
+from airontools.constructors.layers import layer_constructor
+from tensorflow.keras.layers import Input
+
+
+class TestLayerConstructor:
+    def test_output_units(self):
+        units = [10, 5, 2]
+        input_layer = Input(shape=(units[0]))
+        for n_units in units:
+            layer = layer_constructor(x=input_layer, units=n_units)
+            assert n_units == layer.shape[-1]
