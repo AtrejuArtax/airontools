9b3b7266fed367039c814b127d402d1fc1bce95b
Claudi Ruiz Camps <claudi_ruiz@hotmail.com>
2023-04-24 16:09:58 +0200

    classification.py refactoring

--- airontools/constructors/models/supervised/classification.py
+++ airontools/constructors/models/supervised/classification.py
@@ -1,8 +1,9 @@
 import json
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple, Union
 
 import numpy as np
 import tensorflow as tf
+from numpy.typing import NDArray
 
 from airontools.constructors.layers import layer_constructor
 from airontools.constructors.models.model import Model
@@ -10,6 +11,8 @@ from airontools.on_the_fly import HyperDesignDropoutRate
 
 
 class ImageClassifierNN(Model, tf.keras.models.Model):
+    """Image classifier model."""
+
     def __init__(
         self,
         input_shape: Tuple[int],
@@ -48,30 +51,38 @@ class ImageClassifierNN(Model, tf.keras.models.Model):
         tf.keras.models.Model.compile(self, *args, **kwargs)
 
     def fit(self, *args, **kwargs) -> None:
-        """Compile model."""
+        """Fit model."""
         tf.keras.models.Model.fit(self, *args, **kwargs)
 
-    def evaluate(self, x: np.array, y: np.array, **kwargs) -> Dict[str, float]:
+    def evaluate(
+        self, x: NDArray[float], y: NDArray[float], **kwargs
+    ) -> Dict[str, tf.Tensor]:
+        """Evaluate model."""
         if "sample_weight_val" in kwargs.keys():
             sample_weight = kwargs["sample_weight_val"]
         else:
             sample_weight = 1
         return {"loss": self._loss_evaluation(y, self._model(x), sample_weight)}
 
-    def predict(self, *args, **kwargs) -> np.array:
-        """Predict model."""
+    def predict(self, *args, **kwargs) -> NDArray[float]:
+        """Predict."""
         return tf.keras.models.Model.predict(self, *args, **kwargs)
 
     def save_weights(self, path: str) -> None:
+        """Save model weights."""
         with open(path + "_weights", "w") as f:
             json.dump([w.tolist() for w in self._model.get_weights()], f)
 
     def load_weights(self, path: str) -> None:
+        """Load model weights."""
         with open(path + "_weights") as f:
             encoder_weights = [np.array(w) for w in json.load(f)]
         self._model.set_weights(encoder_weights)
 
-    def train_step(self, data, **kwargs) -> dict:
+    def train_step(
+        self, data: Union[List[NDArray[float]]], **kwargs
+    ) -> Dict[str, float]:
+        """Train step."""
         if len(data) == 3:
             x, y, sample_weight = data
         else:
@@ -84,15 +95,17 @@ class ImageClassifierNN(Model, tf.keras.models.Model):
         self.loss_tracker.update_state(loss)
         return {"loss": self.loss_tracker.result()}
 
-    def _loss_evaluation(self, y, y_pred, sample_weight):
+    def _loss_evaluation(
+        self, y: NDArray[float], y_pred: NDArray[float], sample_weight: NDArray[float]
+    ) -> tf.Tensor:
         loss = tf.keras.metrics.categorical_crossentropy(y, y_pred) * sample_weight
         loss = tf.reduce_mean(tf.reduce_sum(loss))
         return loss
 
-    def call(self, inputs) -> None:
+    def call(self, inputs, **kwargs) -> tf.Tensor:
         """Call model."""
-        return self._model(inputs)
+        return self._model(inputs, **kwargs)
 
-    def summary(self) -> None:
+    def summary(self, **kwargs) -> None:
         """Model summary."""
-        self._model.summary()
+        self._model.summary(**kwargs)
