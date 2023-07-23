85f27ea8917b095a6b91bfa5b57d77a5af670e8d
Claudi Ruiz Camps <claudi_ruiz@hotmail.com>
2023-07-23 19:57:44 +0200

    removed unused functions

--- airontools/constructors/utils.py
+++ airontools/constructors/utils.py
@@ -1,8 +1,6 @@
 import warnings
 
-import numpy as np
 import tensorflow as tf
-import tensorflow.python.keras.backend as k_bcknd
 
 
 def get_latent_model(model: tf.keras.models.Model, layer_name: str):
@@ -37,36 +35,6 @@ def set_precision(precision: str) -> None:
             tf.keras.mixed_precision.set_global_policy(policy)
         else:
             tf.keras.backend.set_floatx("float16")
-
-
-def to_time_series(tensor: tf.Tensor) -> tf.Tensor:
-    return k_bcknd.expand_dims(tensor, axis=2)
-
-
-def get_layer_units(
-    input_dim: int,
-    output_dim: int,
-    n_layers: int,
-    min_hidden_units=2,
-) -> list:
-    units = [
-        max(int(units), min_hidden_units)
-        for units in np.linspace(input_dim, output_dim, n_layers + 1)
-    ]
-    units[0], units[-1] = input_dim, output_dim
-    return units
-
-
-def rm_redundant(values: list, value: int) -> list:
-    taken = False
-    values_ = []
-    for n in values:
-        if n != value:
-            values_ += [n]
-        elif not taken:
-            values_ += [n]
-            taken = True
-    return values_
 
 
 def get_regularizer(
