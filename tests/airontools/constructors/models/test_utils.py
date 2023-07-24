957a2022aa148b49ae099ae151ac6ceda0a851cc
Claudi Ruiz Camps <claudi_ruiz@hotmail.com>
2023-07-24 08:35:11 +0200

    TestSetPrecision

--- tests/airontools/constructors/models/test_utils.py
+++ tests/airontools/constructors/models/test_utils.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from airontools.constructors.models.unsupervised.vae import VAE
-from airontools.constructors.utils import get_latent_model
+from airontools.constructors.utils import get_latent_model, set_precision
 from tests.airontools.constructors.models.example_data import TABULAR_DATA
 
 
@@ -14,16 +14,33 @@ class TestGetLatentModel:
     )
     model.compile(optimizer=tf.keras.optimizers.Adam())
 
-    def test_successful_get_latent_model(self):
+    def test_successful_case(self):
         latent_model = get_latent_model(
             model=self.model,
             layer_name="_".join([self.model_name, "encoder"]),
         )
         assert isinstance(latent_model, tf.keras.models.Model)
 
-    def test_unsuccessful_get_latent_model(self):
+    def test_unsuccessful_case(self):
         latent_model = get_latent_model(
             model=self.model,
             layer_name="_".join([self.model_name, "pepito"]),
         )
         assert latent_model is None
+
+
+class TestSetPrecision:
+    def test_float16_case(self):
+        precision = "float16"
+        set_precision(precision)
+        assert tf.keras.backend.floatx() == precision
+
+    def test_mixed_float16_case(self):
+        precision = "mixed_float16"
+        set_precision(precision)
+        assert tf.keras.backend.floatx() == "float32"
+
+    def test_default_case(self):
+        precision = tf.keras.backend.floatx()
+        set_precision(precision)
+        assert tf.keras.backend.floatx() == precision
