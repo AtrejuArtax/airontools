fbfb2f34524c4062fc1a0cf0dc22662987706687
Claudi Ruiz Camps <claudi_ruiz@hotmail.com>
2025-08-10 14:25:18 +0200

    chore: 0.1.62 (#117)
    * chore: 0.1.62
    
    * fix

--- setup.py
+++ setup.py
@@ -6,25 +6,15 @@ import setuptools
 
 # Release new version steps
 # ToDo: crate a script to automate the following commands
-# rm -rf airontools.egg-info/
+# rm -rf *.egg-info/
 # rm -rf build/
 # rm -rf dist/
 # rm requirements.txt
 # poetry export --without-hashes --format=requirements.txt > requirements.txt
-# poetry run python setup.py bdist_wheel
-# poetry run python -m twine upload dist/*
+# poetry build
+# run python -m twine upload dist/*.whl --config-file .pypirc
 
 PACKAGE_NAME = "airontools"
-SUB_PACKAGES_NAMES = [
-    "airontools.constructors",
-    "airontools.constructors.models",
-    "airontools.constructors.models.supervised",
-    "airontools.constructors.models.unsupervised",
-    "airontools.on_the_fly",
-]
-OPTIONS = {}
-if os.uname().sysname.lower() == "darwin":
-    OPTIONS.update({"bdist_wheel": {"plat_name": "macosx_13_0_arm64"}})
 
 with open("README.md") as fh:
     long_description = fh.read()
@@ -35,15 +25,15 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version="0.1.60",
+    version="0.1.62",
     scripts=[],
-    author="Claudi Ruiz Camps",
+    author="Claudi",
     author_email="claudi_ruiz@hotmail.com",
     description="Machine learning tools to complement the AIronSuit package.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AtrejuArtax/airontools",
-    packages=setuptools.find_packages(include=[PACKAGE_NAME] + SUB_PACKAGES_NAMES),
+    packages=setuptools.find_packages(include=[f"{PACKAGE_NAME}*"]),
     install_requires=required,
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -51,5 +41,4 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     license="BSD",
-    options=OPTIONS,
 )
